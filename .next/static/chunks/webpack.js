f3e3c92fd524030d896a7213cf39c7dd9726ff2e
Shahar Assenheim <Shahar.Assenheim@rigaku.com>
2025-12-04 14:19:24 +0200

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "574f527c6cdf3ba1"; }
+/******/ 		__webpack_require__.h = function() { return "3d23883d21427c80"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
